eb492ae471499bc1c8bdd62a6aad6e5ddd67059c
Sergej Alikov <sergej.alikov@gmail.com>
2018-11-03 12:43:23 +0000

    Prevent duplicate entries

--- resources/migrations/20181103115018-slices.up.sql
+++ resources/migrations/20181103115018-slices.up.sql
@@ -1,9 +1,6 @@
 CREATE TABLE IF NOT EXISTS slices
 (
-    "lease-id" uuid REFERENCES leases (id) ON DELETE CASCADE,
-    "offset" integer NOT NULL
+    "lease-id" uuid NOT NULL REFERENCES leases (id) ON DELETE CASCADE,
+    "offset" integer NOT NULL,
+    CONSTRAINT slices_pkey PRIMARY KEY ("lease-id", "offset")
 );
---;;
-CREATE INDEX "lease-id"
-    ON slices USING btree
-    ("lease-id");
