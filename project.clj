26e14573da8d8e2a46f18e679827e0db8e3e15a3
Sergej Alikov <sergej.alikov@gmail.com>
2018-11-25 01:08:17 +0000

    Start 2.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trail "2.0.0-SNAPSHOT"
+(defproject trail "2.0.1-SNAPSHOT"
   :description "IP history management API"
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [metosin/compojure-api "1.1.11"]
