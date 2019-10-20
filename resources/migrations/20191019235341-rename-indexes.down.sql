9b8bc8ed35719e1c3366a7e39df2beca73e4a09d
Sergej Alikov <sergej.alikov@gmail.com>
2019-10-20 01:09:47 +0100

    TRAIL-104 Add missing prefix to some indexes

---
+++ resources/migrations/20191019235341-rename-indexes.down.sql
@@ -0,0 +1,7 @@
+ALTER INDEX "leases_end-date" RENAME TO "end-date";
+--;;
+ALTER INDEX leases_ip RENAME TO ip;
+--;;
+ALTER INDEX "leases_ip-start-date" RENAME TO "ip-start-date";
+--;;
+ALTER INDEX "leases_start-date" RENAME TO "start-date";
---
+++ resources/migrations/20191019235341-rename-indexes.up.sql
@@ -0,0 +1,7 @@
+ALTER INDEX "end-date" RENAME TO "leases_end-date";
+--;;
+ALTER INDEX ip RENAME TO leases_ip;
+--;;
+ALTER INDEX "ip-start-date" RENAME TO "leases_ip-start-date";
+--;;
+ALTER INDEX "start-date" RENAME TO "leases_start-date";
