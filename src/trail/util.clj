66a10ba322a1d5b0e9bb437dc0b59e62dc6f7473
Sergej Alikov <sergej.alikov@gmail.com>
2019-10-23 00:28:09 +0100

    Move version into the VERSION file

--- src/trail/util.clj
+++ src/trail/util.clj
@@ -1,14 +1,4 @@
 (ns trail.util
   (:require [clojure.java.io :as io]))
 
-(defn pom-properties
-  []
-  (try
-    (with-open [reader (-> "META-INF/maven/trail/trail/pom.properties"
-                           io/resource
-                           io/reader)]
-      (doto (java.util.Properties.)
-        (.load reader)))
-    (catch java.io.FileNotFoundException e)))
-
-(def version (get (pom-properties) "version"))
+(def version (clojure.string/trim-newline (slurp (io/resource "VERSION"))))
