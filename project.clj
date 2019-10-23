4c522876dc4488d3db5c0acfc7a04254a7442e40
Sergej Alikov <sergej.alikov@gmail.com>
2019-10-23 01:02:41 +0100

    Add symlink to VERSION for easier access

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject trail (clojure.string/trim-newline (slurp "resources/VERSION"))
+(defproject trail (clojure.string/trim-newline (slurp "VERSION"))
   :description "IP history management API"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [metosin/compojure-api "1.1.12"]
